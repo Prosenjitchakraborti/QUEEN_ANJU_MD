6578b511e86dcc987708ba5f617ed59cb6bc0c80
Prosenjitchakraborti <prosenjitchakraborty375@gmail.com>
2024-11-26 03:29:47 +0530

    Update session.js

--- session.js
+++ session.js
@@ -21,5 +21,5 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "your session_id here",
+SESSION_ID: process.env.SESSION_ID || "ANJU-MD=OFkUFYbL#QMYDAEco0H32PsxwdMXw53Gm12KH1CjGVci8i-uQIIs",
 };
